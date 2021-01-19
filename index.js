dc7954548b05604f3e3616967674d1e32651abe8
Natalia Carvajal <nataliacarvajal1996@gmail.com>
2021-01-19 10:02:43 -0500

    readded generatemarkdown file

--- index.js
+++ index.js
@@ -58,16 +58,16 @@ inquirer
       : console.log("Your ReadMe has been generated!");
   });
 
-// // TODO: Include packages needed for this application
+// TODO: Include packages needed for this application
 
-// // TODO: Create an array of questions for user input
-// const questions = [];
+// TODO: Create an array of questions for user input
+const questions = [];
 
-// // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
 
-// // TODO: Create a function to initialize app
-// function init() {}
+// TODO: Create a function to initialize app
+function init() {}
 
-// // Function call to initialize app
-// init();
+// Function call to initialize app
+init();
